c84ea387cc6ffff87ffb8e78c02485214811f483
Giorgio Matessi <giorgio.matessi@gmail.com>
2022-07-25 12:42:11 -0600

    Added headers to CSV files

--- Code/Supplementary/convertToCSV.py
+++ Code/Supplementary/convertToCSV.py
@@ -1,6 +1,8 @@
 import csv
 import os
 
+from numpy import number
+
 """
 Created on Fri July, 22 2022 11:14:35
 Convert Data Files to CSV files
@@ -11,11 +13,14 @@ Notes:
 
 """
 
+
+
 inputDirPath = input("Path of Input Directory: ")
 
 if '\\' in inputDirPath or '\"' in inputDirPath:
     inputDirPath.replace('\\','/')
     inputDirPath.replace('\"','')
+    
 
 if not os.path.isdir(inputDirPath):
     print("Directory does not exist.")
@@ -26,7 +31,7 @@ outputDirPath = input("Output Directory: ")
 if '\\' in outputDirPath or '\"' in outputDirPath:
     outputDirPath.replace('\\','/')
     outputDirPath.replace('\"','')
-
+    outputDirPath.replace('\"','')
 
 
 # Checks for output folder existance 
@@ -38,13 +43,14 @@ if not os.path.isdir(outputDirPath):
 
 for filename in os.listdir(inputDirPath): #Runs through all files in a directory
     with open(os.path.join(inputDirPath, filename), 'r') as csv_file: 
-        csv_reader = csv.reader(csv_file, delimiter=' ') # Reads file 
+        csv_reader = csv.DictReader(csv_file, delimiter=' ') # Reads file 
 
         name = outputDirPath + '/' + os.path.basename(filename) + ".csv" # Stores name of file
 
         with open(name, 'w', newline='') as new_file: 
+            new_file.write("First,Second,Third,Fourth\n")
             csv_writer = csv.writer(new_file, delimiter=',') # Rewrites file to have a comma delimiter 
-        
+            
             for line in csv_reader:
                 csv_writer.writerow(line)
 
